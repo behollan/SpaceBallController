57e68545d857cb318a795649ead3bb284787f392
Ben Holland <benjamin.holland1@gmail.com>
2017-06-28 07:30:28 -0600

    Added comments for clarity
    Added comments for code clarity. Added TODO list

--- SpaceBallController/SpaceBallController.ino
+++ SpaceBallController/SpaceBallController.ino
@@ -1,5 +1,22 @@
-#include <IridiumSBD.h>
+/*TODO::
+- Setup Iridium transmission in main loop
+- Heater feedback loop
+  - Must buy 2 FET's
+- Add servo control for cooler
+*/
+//Library Includes
+#include <IridiumSBD.h> //Download from https://github.com/mikalhart/IridiumSBD
 #include <Wire.h> //I2C library, reserves A4 and A5 for I2C
+#include <SoftwareSerial.h>
+
+//Function declarations
+float * tempMeasure();
+byte fetch_humidity_temperature(unsigned int *p_Humidity, unsigned int *p_Temperature);
+void print_float(float f, int num_digits);
+
+//Variables defines
+#define TRUE 1
+#define FALSE 0
 
 // Analog Temp Sensor Pins
 int temp1 = A0;
@@ -8,23 +25,14 @@ int temp3 = A2;
 int temp4 = A3;
 
 
-float * tempMeasure();
-byte fetch_humidity_temperature(unsigned int *p_Humidity, unsigned int *p_Temperature);
-void print_float(float f, int num_digits);
-
-#define TRUE 1
-#define FALSE 0
-
-#include <SoftwareSerial.h>
-
+//Object creation
 SoftwareSerial OpenLog(2, 3); // RX, TX
-SoftwareSerial ssIridium(4, 5); // RockBLOCK serial port on 18/19
-
+SoftwareSerial ssIridium(4, 5); // RockBLOCK serial port on 4/5 (RX, TX)
 IridiumSBD isbd(ssIridium, 6);   // RockBLOCK SLEEP pin on 10
 
 void setup() {
-  Serial.begin(9600);
-  OpenLog.begin(9600);
+  Serial.begin(9600); //Serial monitor
+  OpenLog.begin(9600); //Data logging
 }
 
 void loop() {
