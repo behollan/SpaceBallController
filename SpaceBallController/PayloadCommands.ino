cccdde707472aff0e90e30b29fad803b2b91fa71
Ben Holland <benjamin.holland1@gmail.com>
2017-07-06 18:22:47 -0600

    Added temp sensor functionality

--- SpaceBallController/PayloadCommands.ino
+++ SpaceBallController/PayloadCommands.ino
@@ -3,18 +3,26 @@ float * tempMeasure(){ //Measures temperature from 4 temp sensors and I2C sensor
   unsigned int H_dat, T_dat;
   float RH, T_C;
 
-  _status = fetch_humidity_temperature(&H_dat, &T_dat);
+  //_status = fetch_humidity_temperature(&H_dat, &T_dat);
 
-  T_C = (float) T_dat * 1.007e-2 - 40.0;
+  //T_C = (float) T_dat * 1.007e-2 - 40.0;
   
   static float data[5]; //Initialize a data array for temp storage
   data[0] = analogRead(temp1);
   data[1] = analogRead(temp2);
   data[2] = analogRead(temp3);
   data[3] = analogRead(temp4);
-  data[4] = T_C;
-  return(data);
+  data[4] = analogRead(temp5);
+  //data[4] = T_C;
+  
+  int arrayLength = sizeof(data)/sizeof(data[0]);
+  
+  for (int i=0; i < arrayLength; i++){ //For each element of the array, convert to voltage then to temperature
+    data[i]= 100*(data[i]*5/1024-0.5) ;
+  }
+  return data;
 }
+/*
 float humidity(){ //Reads and returns HIH6130 relative humidity
   byte _status;
   unsigned int H_dat, T_dat;
@@ -26,4 +34,5 @@ float humidity(){ //Reads and returns HIH6130 relative humidity
 
   return(RH);
 }
+*/
 
--- SpaceBallController/SpaceBallController.ino
+++ SpaceBallController/SpaceBallController.ino
@@ -8,6 +8,7 @@
 #include <IridiumSBD.h> //Download from https://github.com/mikalhart/IridiumSBD
 #include <Wire.h> //I2C library, reserves A4 and A5 for I2C
 #include <SoftwareSerial.h>
+#include <stdio.h>
 
 //Function declarations
 float * tempMeasure();
@@ -23,29 +24,65 @@ int temp1 = A0;
 int temp2 = A1;
 int temp3 = A2;
 int temp4 = A3;
+int temp5 = A4;
 
 
 //Object creation
-SoftwareSerial OpenLog(2, 3); // RX, TX
-SoftwareSerial ssIridium(4, 5); // RockBLOCK serial port on 4/5 (RX, TX)
-IridiumSBD isbd(ssIridium, 6);   // RockBLOCK SLEEP pin on 10
+//SoftwareSerial OpenLog(2, 3); // RX, TX
+//SoftwareSerial ssIridium(4, 5); // RockBLOCK serial port on 4/5 (RX, TX)
+//IridiumSBD isbd(ssIridium, 6);   // RockBLOCK SLEEP pin on 10
 
 void setup() {
   Serial.begin(9600); //Serial monitor
-  OpenLog.begin(9600); //Data logging
+  //OpenLog.begin(9600); //Data logging
+  Serial.println("Init Complete. Printing temp data.");
 }
 
-void loop() {
-  ISBDCallback(){ //Place things here to run during the transmit attempt
+void loop() {    
+    float *tempPoint;
+    tempPoint = tempMeasure(); //Grab the tempMeasure pointer
+    
+    //Build array of temperatureData
+    float tempData [5] = {*(tempPoint + 0),*(tempPoint + 1),*(tempPoint + 2),*(tempPoint + 3),*(tempPoint + 4)}; 
+    int arrayLength = sizeof(tempData)/sizeof(tempData[0]);
+
+    //Serial.println(*(tempPoint + 0));
+    
+    //Serial.println(tempPoint);
+    
+    for (int i=0; i < arrayLength; i++){ //For each element of the array, print and log it
+      Serial.print(tempData[i]);
+      Serial.print(", ");
+      
+      //OpenLog.print(tempData[i]);
+      //OpenLog.print(",");
+    }
+    //float hum = humidity(); //Get humidity data
+    //OpenLog.println( hum ); //Log humidity 
+    //Serial.println( hum ); //Print humidity
+    Serial.println("");
+    delay(100);
+}
+
+/*
+bool  ISBDCallback(){ //Place things here to run during the transmit attempt
     //In this case, keep logging data during the transmission
-    Serial.print(* tempMeasure() );
-    Serial.print(" ");
-    Serial.println( humidity() );
-  
-    OpenLog.print(* tempMeasure());
-    OpenLog.print(" ");
-    OpenLog.println( humidity() );
+    float *tempPoint;
+    tempPoint = tempMeasure(); //Grab the tempMeasure pointer
+    
+    //Build array of temperatureData
+    float tempData [5] = {*(tempPoint + 0),*(tempPoint + 1),*(tempPoint + 2),*(tempPoint + 3),*(tempPoint + 4)}; 
+    int arrayLength = sizeof(tempData)/sizeof(tempData[0]);
+    
+    for (int i=0; i < arrayLength; i++){ //For each element of the array, print and log it
+      Serial.print(tempData[i]);
+      Serial.print(", ");
+      
+      OpenLog.print(tempData[i]);
+      OpenLog.print(",");
+    }
+    float hum = humidity(); //Get humidity data
+    OpenLog.println( hum ); //Log humidity 
+    Serial.println( hum ); //Print humidity
   }
-
-  
-}
+  */
